96f7c4be539532d065c724835b28dd7d1dd1f686
Jonathan Dyason <jonathandyason@hotmail.com>
2021-09-28 17:20:18 +0200

    data

---
+++ Models/Department.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Department
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Manager { get; set; }
+    public string Status { get; set; }
+}
