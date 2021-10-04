5e341f1c65926a86ae4036f5fe445b606bbf7362
Jonathan Dyason <jonathandyason@hotmail.com>
2021-10-04 09:03:08 +0200

    employee

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { EmployeeListComponent } from './employeeList/employeeList.component';
 import { EmployeeCreateComponent } from './employeeCreate/employeeCreate.component';
+import { EmployeeDetailComponent } from './employeeDetail/employeeDetail.component';
 import { DepartmentListComponent } from './departmentList/departmentList.component';
 import { DepartmentCreateComponent } from './departmentCreate/departmentCreate.component';
 import { DepartmentDetailComponent } from './departmentDetail/departmentDetail.component';
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
   { path: 'employee-list', component: EmployeeListComponent},
-  { path: 'employee-create', component: EmployeeCreateComponent},
+  { path: 'employee-create', component: EmployeeCreateComponent },
+  { path: 'employees/:id', component: EmployeeDetailComponent },
   { path: 'department-list', component: DepartmentListComponent},
   { path: 'department-create', component: DepartmentCreateComponent},
   { path: 'departments/:id', component: DepartmentDetailComponent}
---
+++ ClientApp/src/app/employeeDetail/employeeDetail.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { EmployeeService } from '../employee/employee.service';
+import { Employee } from '../employee/employee';
+
+@Component({
+  selector: 'app-employeeDetail',
+  templateUrl: './employeeDetail.component.html'
+})
+
+export class EmployeeDetailComponent implements OnInit {
+  employee: Employee | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private employeeService: EmployeeService,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.getEmployee();
+  }
+
+  getEmployee(): void {
+    const id = parseInt(this.route.snapshot.paramMap.get('id')!, 10);
+    this.employeeService.getEmployee(id)
+      .subscribe(employee => this.employee = employee);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  //save(): void {
+  //  if (this.employee) {
+  //    this.employeeService.updateEmployee(this.employee)
+  //      .subscribe(() => this.goBack());
+  //  }
+  //}
+
+}
+
+
+
